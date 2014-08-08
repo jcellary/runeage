97c5d04a5171f18c1ebac6da93418596b17b9a4f
Jarosław Cellary <jaroslaw@wikia-inc.com>
2014-08-08 10:11:27 +0200

    added welcome screen

--- js/runeage.js
+++ js/runeage.js
@@ -1,53 +1,57 @@
 var Game = {
 
-    welcomeScreen:
-        '<H1>Welcome to RuneAge!</H1>' +
+    main: function(id) {
+        View.mainPanel = $(id);
+
+        View.loadImages();
+        View.displayWelcome();
+    },
+
+    startGame: function() {
+    
+        View.displayGame();
+    }
+};
+
+var View = {
+
+    welcomeTemplate:
+        '<div id="WelcomeScreen">' +
+        '<br>' +
+        '<h1>Welcome to Rune Age!</h1>' +
         'Please provide your name: ' +
-        '<Form>' +
-        '<Input Type="Text" Id="Name" />' +
-        '<Input Type="Button" onclick="Game.submitWelcome()" />' +
-        '</Form>',
+        '<form>' +
+        '<input type="Text" id="TextName" />' +
+        '<input type="Button" value="Start" id="ButtonStart" />' +
+        '</form>' +
+        '</div>',
 
-    start: function(id) {
-        this.mainPanel = $(id);
+    gameTemplate:
+        '',
+        
+    displayView: function(template) {
+        this.mainPanel.html('');
+        this.mainPanel.append(template);
+    
+    },
 
-        this.loadImages();
-        this.displayWelcome();
-        this.runGame();
+    displayWelcome: function() {
+        this.displayView(this.welcomeTemplate);
+        
+        $('#ButtonStart').click(function() { Game.startGame(); });
+    },
+    
+    displayGame: function() {
+        this.displayView(this.gameTemplate);
+        
+        var cardDiv = $('<div id = "card1" />').css('width', 160).css('height', 244);
+        cardDiv.css('background-image', 'url(' + this.imgCardsUndead.src + ')').css('background-position', '-320px 0px');
 
-
+        this.mainPanel.append(cardDiv);
     },
 
     loadImages: function () {
         this.imgCardsUndead = new Image();
         this.imgCardsUndead.src = 'img/cards_undead.jpg';
-
-        //this.imgCardsUndead1 = this.imgCardsUndead.copy;
     },
-
-    displayWelcome: function() {
-        mainPanel.clear();
-
-        mainPanel.append(this.welcomeScreen);
-
-    },
-
-    submitWelecome: function() {
-      this.runGame();
-    },
-
-    runGame: function() {
-        mainPanel.clear();
-
-        //mainPanel.backgroundImage = this.imgCardsUndead;
-        var cardDiv = $('<div id = "card1" />').css('width', 100).css('height', 100).css('background-color', '#00ff00');
-        cardDiv.append(this.imgCardsUndead);
-
-        //cardDiv.backgroundImage = this.imgCardsUndead;
-        //cardDiv.css('background', this.imgCardsUndead).css('background-position', '0, 0');
-
-        mainPanel.append(cardDiv);
-
-    }
-
-};
+}
